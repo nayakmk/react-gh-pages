92ef5f76a19955683c4ec0d57cfec871d11b4baa
Malaya Nayak <malaya.nayak@outlook.com>
2024-12-01 02:10:19 +0530

    test new repo

--- next.config.js
+++ next.config.js
@@ -2,9 +2,23 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    unoptimized: true,
+    remotePatterns: [],
+  },
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  generateBuildId: async () => {
+    return process.env.BUILD_ID || 'development'
+  },
   experimental: {
+    optimizePackageImports: ['@icons-pack/react-simple-icons'],
     appDir: true,
   },
+  transpilePackages: [],
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = withContentlayer(nextConfig);
---
+++ out/_next/static/chunks/app/layout-2b81145e9337b186.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5532:function(n,e,t){Promise.resolve().then(t.t.bind(t,7477,23)),Promise.resolve().then(t.t.bind(t,7296,23))},7296:function(){}},function(n){n.O(0,[477,253,769,744],function(){return n(n.s=5532)}),_N_E=n.O()}]);
