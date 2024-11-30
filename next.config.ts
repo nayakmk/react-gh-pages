6d47a4fbbcb6a119361e308046a7d9e9c9302ba1
Malaya Nayak <malaya.nayak@outlook.com>
2024-11-30 20:06:06 +0530

    some more config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Enable static exports
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
